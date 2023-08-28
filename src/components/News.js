d15848cd4d24cff6f71454dcb77d29389789e4c3
Niraj Kumar <1751nk@gmail.com>
2023-08-28 13:30:29 +0530

    lecture 25 finished

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ export class News extends Component {
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row mt-4">
         <div className="col-md-4">
-        <NewsItem title="myTitle" description="myDescription"/>
+        <NewsItem title="myTitle" description="myDescription" imageurl="https://images.idgesg.net/images/article/2017/08/ubuntu-100734185-large.jpg?auto=webp&quality=85,70"/>
         </div>
         <div className="col-md-4">
         <NewsItem title="myTitle" description="myDescription"/>

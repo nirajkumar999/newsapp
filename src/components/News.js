c4bb50e00df7610c90bd9142f955da4b8780bab4
Niraj Kumar <1751nk@gmail.com>
2023-08-28 17:30:27 +0530

    lecture 29 : added loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner'
 
 export class News extends Component {
 
@@ -13,42 +14,46 @@ export class News extends Component {
     }
 
     async componentDidMount(){ 
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=1pageSize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true})
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData); 
-        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false})
     }
 
      handlePrevClick = async ()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=${this.state.page - 1}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json()
-        console.log(parsedData);  
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles
+            articles: parsedData.articles,
+            loading: false
         })
 
     }
     
      handleNextClick = async ()=>{
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=${this.state.page + 1}&pageSize=20`;
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e4f52cf2b43f4a8d9e98d37368bd407e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            this.setState({loading:true});
             let data = await fetch(url);
-            let parsedData = await data.json()
-            console.log(parsedData);  
+            let parsedData = await data.json() 
             this.setState({
                 page: this.state.page + 1,
-                articles: parsedData.articles
+                articles: parsedData.articles,
+                loading: false
             })
     }
 
     render() { 
         return (
             <div className="container my-3">
-                <h1>NewsMonkey - Top Headlines</h1> 
+                <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+                {this.state.loading && <Spinner/>} 
                 <div className="row"> 
-                {this.state.articles.map((element)=>{
+                {!this.state.loading && this.state.articles.map((element)=>{
                     return <div className="col-md-4" key={element.url}>
                         <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div> 
@@ -56,7 +61,7 @@ export class News extends Component {
                 </div> 
                 <div className="container d-flex justify-content-center">
                 <button disabled={this.state.page<=1} type="button" className="btn btn-dark mx-3" onClick={this.handlePrevClick} style={{ width: '110px'}}> &larr; Previous</button>
-                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.handleNextClick} style={{ width: '110px' }}>Next &rarr;</button>
+                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} style={{ width: '110px' }}>Next &rarr;</button>
                 </div>
             </div>
         )
---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import _loading from './_loading.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img src={_loading} alt="loading" />
+      </div>
+    )
+  }
+}
+
+export default Spinner

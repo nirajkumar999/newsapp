1066dea8e937393d0db3bc81116c03df00d01455
Niraj Kumar <1751nk@gmail.com>
2023-08-28 10:47:36 +0530

    lecture 23 finished

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        this is newsitem
+      </div>
+    )
+  }
+}
+
+export default NewsItem

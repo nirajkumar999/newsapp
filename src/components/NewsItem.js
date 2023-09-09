826f426c4f0285c8a42d61f3f791ba3ad9a32039
Niraj Kumar <1751nk@gmail.com>
2023-09-09 17:35:01 +0530

    lecture 32 : added badge, author and time to my newsitem card

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import default_image from "../default_image.png"
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let {title,description,imageUrl,newsUrl, author, date, source} = this.props;
     return (
       <div className="my-3">
       <div className="card">
+      <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{fontSize: '15px', left:'50%', zIndex:'1'}}> {source}</span>
         <img src={!imageUrl?default_image:imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By : {!author?"Unknown author":author} on {!date?"(Date : Unavailable)": new Date(date).toLocaleString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More...</a>
           </div>
       </div>

017092d18557a99acfc96126ed0e3255eb200266
Niraj Kumar <1751nk@gmail.com>
2023-10-08 21:28:16 +0530

    lec 36: added top loading bar and scroll to top

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import "./App.css";
-
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
-  pageSize=12;
+  pageSize = 12;
+  state={
+    progress:0
+  }
+  setProgress = (progress) => { this.setState({progress: progress})}
   render() {
     return (
       <>
-      <BrowserRouter>
-        <Navbar/>
-        <div>
-        <Routes>
-          
-            <Route exact path="/" element={<News key='general' pageSize={this.pageSize} country='in'/>}></Route>
+        <BrowserRouter>
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+            height={4}
+          />
+          <Navbar />
+          <div>
+            <Routes>
 
-            <Route exact path="/business" element={<News key='business' pageSize={this.pageSize} country='in' category='business'/>}></Route>
+              <Route exact path="/" element={<News setProgress={this.setProgress} key='general' pageSize={this.pageSize} country='in' />}></Route>
 
-            <Route exact path="/entertainment" element={<News key='entertainment' pageSize={this.pageSize} country='in' category='entertainment'/>}></Route>
+              <Route exact path="/business" element={<News setProgress={this.setProgress} key='business' pageSize={this.pageSize} country='in' category='business' />}></Route>
 
-            <Route exact path="/health" element={<News key='health' pageSize={this.pageSize} country='in' category='health'/>}></Route>
+              <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key='entertainment' pageSize={this.pageSize} country='in' category='entertainment' />}></Route>
 
-            <Route exact path="/science" element={<News key='science' pageSize={this.pageSize} country='in' category='science'/>}></Route>
+              <Route exact path="/health" element={<News setProgress={this.setProgress} key='health' pageSize={this.pageSize} country='in' category='health' />}></Route>
 
-            <Route exact path="/sports" element={<News key='sports' pageSize={this.pageSize} country='in' category='sports'/>}></Route>
+              <Route exact path="/science" element={<News setProgress={this.setProgress} key='science' pageSize={this.pageSize} country='in' category='science' />}></Route>
 
-            <Route exact path="/technology" element={<News key='technology' pageSize={this.pageSize} country='in' category='technology'/>}></Route>
+              <Route exact path="/sports" element={<News setProgress={this.setProgress} key='sports' pageSize={this.pageSize} country='in' category='sports' />}></Route>
 
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
+              <Route exact path="/technology" element={<News setProgress={this.setProgress} key='technology' pageSize={this.pageSize} country='in' category='technology' />}></Route>
+
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </>
     );
   }
 }
 
-// to every <News> component, we need to pass a unique key parameter so that each time we hit our end points the complete components get re rendered with the updated props value. Otherwise this wont work and we will see the same page on our each endpoint
+// to every <News setprogress={this.setProgress}> component, we need to pass a unique key parameter so that each time we hit our end points the complete components get re rendered with the updated props value. Otherwise this wont work and we will see the same page on our each endpoint
--- src/components/News.js
+++ src/components/News.js
@@ -3,10 +3,13 @@ import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { Button } from 'react-bootstrap';
 
 
-//const apiKey = 'c8f17bd15a7d4b45be77b4d0cc0a614c';
-const apiKey = 'e4f52cf2b43f4a8d9e98d37368bd407e';
+const apiKey = 'c8f17bd15a7d4b45be77b4d0cc0a614c';
+//const apiKey = 'e4f52cf2b43f4a8d9e98d37368bd407e';
 //const apiKey = 'e42188c058484d74b08bd58fed2bde25';
 
 function getCategoryIcon(category) {
@@ -39,10 +42,10 @@ const dateOptions = {
     minute: '2-digit',
     second: '2-digit',
     timeZoneName: 'short',
-  };
+};
 
 export class News extends Component {
-    
+
     static defaultProps = {
         country: 'in',
         pageSize: 9,
@@ -68,28 +71,45 @@ export class News extends Component {
         }
         document.title = `${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} - NewsMonkey`
     }
-    
-    async updateNews(){
+
+    scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // For smooth scrolling
+        });
+    };
+
+    async updateNews() {
 
         const { signal } = this.abortController;
         // Set the User-Agent header
         const headers = {
-                'User-Agent': 'NewsMonkey/1.0',
-                'Authorization': `Bearer ${apiKey}`,
+            'User-Agent': 'NewsMonkey/1.0',
+            'Authorization': `Bearer ${apiKey}`,
         };
-        
+
+        this.props.setProgress(10);
+
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
 
         this.setState({ loading: true })
-        let data = await fetch(url,{ signal, headers });
+
+        let data = await fetch(url, { signal, headers });
+
+        this.props.setProgress(30);
 
         let parsedData = await data.json();
+
+        this.props.setProgress(70);
+
         console.log(parsedData);
         this.setState({
             articles: parsedData.articles || [],
             totalResults: parsedData.totalResults,
             loading: false,
         });
+        this.props.setProgress(100);
+
 
     }
 
@@ -105,71 +125,72 @@ export class News extends Component {
         clearInterval(this.intervalId);
     }
 
-    fetchMoreData = async () => {  
-        this.setState({page: this.state.page + 1})
-        const { signal } = this.abortController;
-        // Set the User-Agent header
-        const headers = {
-                'User-Agent': 'NewsMonkey/1.0',
-                'Authorization': `Bearer ${apiKey}`,
-        };
-        console.log("fetch1", this.state.page)
+    fetchMoreData = async () => {
+
+        this.setState({ page: this.state.page + 1 })
+
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        console.log("fetch2", this.state.page)
+
         this.setState({ loading: true })
-        let data = await fetch(url,{ signal, headers });
+        let data = await fetch(url);
 
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
             articles: this.state.articles.concat(parsedData.articles),
             totalResults: parsedData.totalResults,
-            loading:false
+            loading: false
         });
-      };
-    
+    };
+
     render() {
         const dateStr = this.state.currentDate.toLocaleString('en-IN', dateOptions);
-        
+
         return (
 
-            
+
             <>
-                <h1 className='text-center' style={{ margin: '35px 0px' }}>
-                    NewsMonkey - Top Headlines - {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}
-                    <i className={`fas ${getCategoryIcon(this.props.category)}`} style={{ marginLeft: '12px', fontSize: '50px' }}></i>
-                </h1>
+                    <h1 className='text-center' style={{ margin: '35px 0px' }}>
+                        NewsMonkey - Top Headlines - {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}
+                        <i className={`fas ${getCategoryIcon(this.props.category)}`} style={{ marginLeft: '12px', fontSize: '50px' }}></i>
+                    </h1>
 
-                <div className="text-center">
-                    <p style={{ fontWeight: 'bold', fontSize: '22px' }}>
-                        <i className="fas fa-calendar-alt" style={{ marginRight: '8px', fontSize: '30px'}}></i>
-                        {dateStr.replace('IST', '(IST)')}
-                    </p>
-                </div>
-
-                {this.state.loading && <Spinner />}
-                
-                <InfiniteScroll
-                    dataLength={this.state.articles.length}
-                    next={this.fetchMoreData}
-                    //use < instead of !==
-                    hasMore={this.state.articles.length < this.state.totalResults}
-                    loader={this.state.loading && <Spinner/>}
-                > 
-                    <div className="container">
-
-                    <div className="row">
-                        {this.state.articles.map((element,index) => {
-                            return <div className="col-md-4" key={index}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                            </div>
-                        })}
+                    <div className="text-center">
+                        <p style={{ fontWeight: 'bold', fontSize: '22px' }}>
+                            <i className="fas fa-calendar-alt" style={{ marginRight: '8px', fontSize: '30px' }}></i>
+                            {dateStr.replace('IST', '(IST)')}
+                        </p>
                     </div>
-                    </div> 
-                </InfiniteScroll>
-               
 
-               
+                    {this.state.loading && <Spinner />}
+
+                    <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchMoreData}
+                        //use < instead of !==
+                        hasMore={this.state.articles.length < this.state.totalResults}
+                        loader={this.state.loading && <Spinner />}
+                    >
+                        <div className="container">
+
+                            <div className="row">
+                                {this.state.articles.map((element, index) => {
+                                    return <div className="col-md-4" key={index}>
+                                        <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                    </div>
+                                })}
+                            </div>
+                        </div>
+                    </InfiniteScroll>
+
+
+                {this.state.loading === false && this.state.articles.length >= this.state.totalResults && (
+                    <div className="text-center">
+                        <Button variant="primary" onClick={this.scrollToTop}>
+                            Back To Top &nbsp;<FontAwesomeIcon icon={faCircleUp} beat size="xl" />
+                        </Button>
+                    </div>
+                )}
             </>
         )
     }
